d2f53c7ef2f131d858de40ddadd9df6e8d89ed31
Todd Burns <todd@Todds-MacBook-Pro.local>
2026-02-07 11:23:03 -0800

    Use basic PrismaClient

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,12 +4,6 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-function createPrismaClient() {
-  return new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
-  });
-}
-
-export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
